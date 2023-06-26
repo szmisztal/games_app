eb61b2537754c617b050072a7dcd47afd02adf0f
szmisztal <sz.misztal@gmail.com>
2023-06-26 16:06:34 +0200

    add views for homepage, games list and single game

--- games_app/views.py
+++ games_app/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Game
 
-def home_page(request):
-    return HttpResponse("entry site test")
+def homepage(request):
+    return render(request, "home.html")
+
+def games_list(request):
+    games = Game.objects.all()
+    return render(request, "games_list.html", {"games": games})
+
+def single_game(request, game_id):
+    game = get_object_or_404(Game, pk = game_id)
+    return render(request, "single_game.html", {"game": game})
+
