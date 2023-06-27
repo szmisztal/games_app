2182cc0dd19daec32379434b84e96ef92c64e9b9
szmisztal <sz.misztal@gmail.com>
2023-06-27 18:00:34 +0200

    change views to use django generic views

--- games_app/views.py
+++ games_app/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Game
+from .forms import NewGameForm
+from django.views import generic
 
-def homepage(request):
-    return render(request, "home.html")
+class HomepageView(generic.TemplateView):
+    template_name = "home.html"
 
-def games_list(request):
-    games = Game.objects.all()
-    return render(request, "games_list.html", {"games": games})
+class GamesListView(generic.ListView):
+    template_name = "games_list.html"
+    context_object_name = "games"
 
-def single_game(request, game_id):
-    game = get_object_or_404(Game, pk = game_id)
-    return render(request, "single_game.html", {"game": game})
+    def get_queryset(self):
+        return Game.objects.order_by("developer", "title")
+
+class SingleGameView(generic.DetailView):
+    model = Game
+    context_object_name = "game"
+    template_name = "single_game.html"
+
+class AddNewGame(generic.CreateView):
+    model = Game
+    form_class = NewGameForm
+    template_name = "add_game.html"
+    success_url = "/games_app/games_list/"
+
+
+
+
+
 
