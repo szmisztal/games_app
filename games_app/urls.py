ac7645a347b583dcd101bd2fc31da7442048367e
szmisztal <sz.misztal@gmail.com>
2023-06-28 14:05:51 +0200

    add urls for edit objects in db

--- games_app/urls.py
+++ games_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomepageView, GamesListView, DevStudioListView, SingleGameView, SingleDevView, AddNewGame, AddDev
+from .views import HomepageView, GamesListView, DevStudioListView, SingleGameView, SingleDevView, AddNewGame, AddNewDev, EditGame, EditDev
 
 urlpatterns = [
     path("", HomepageView.as_view(), name = "homepage"),
@@ -8,5 +8,7 @@ urlpatterns = [
     path("single_game/<int:pk>/", SingleGameView.as_view(), name = "single_game"),
     path("single_dev/<int:pk>/", SingleDevView.as_view(), name = "single_dev"),
     path("add_game/", AddNewGame.as_view(), name = "add_game"),
-    path("add_dev/", AddDev.as_view(), name = "add_dev")
+    path("add_dev/", AddNewDev.as_view(), name = "add_dev"),
+    path("edit_game/<int:pk>/", EditGame.as_view(), name = "edit_game"),
+    path("edit_dev/<int:pk>/", EditDev.as_view(), name = "edit_dev")
 ]
