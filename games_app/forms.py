cbbf68f686c4a9551a51baabb4ee48b240f48804
szmisztal <sz.misztal@gmail.com>
2023-06-28 12:20:00 +0200

    add form for new dev object

--- games_app/forms.py
+++ games_app/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Game
+from .models import Game, DevelopmentStudio
 
 class NewGameForm(forms.ModelForm):
     class Meta:
         model = Game
         fields = ['title', 'genre', 'short_description', 'premiere', 'developer', 'completed']
+
+class NewDevForm(forms.ModelForm):
+    class Meta:
+        model = DevelopmentStudio
+        fields = ['name']
