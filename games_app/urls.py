c595dba45a6f96983adfe5f0756a7e9dd3ebdac3
szmisztal <sz.misztal@gmail.com>
2023-06-28 12:21:29 +0200

    add new urls

--- games_app/urls.py
+++ games_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import HomepageView, GamesListView, SingleGameView, AddNewGame
+from .views import HomepageView, GamesListView, DevStudioListView, SingleGameView, SingleDevView, AddNewGame, AddDev
 
 urlpatterns = [
     path("", HomepageView.as_view(), name = "homepage"),
     path("games_list/", GamesListView.as_view(), name = "games_list"),
+    path("dev_studios/", DevStudioListView.as_view(), name = "devs_list"),
     path("single_game/<int:pk>/", SingleGameView.as_view(), name = "single_game"),
-    path("add_game/", AddNewGame.as_view(), name = "add_game")
+    path("single_dev/<int:pk>/", SingleDevView.as_view(), name = "single_dev"),
+    path("add_game/", AddNewGame.as_view(), name = "add_game"),
+    path("add_dev/", AddDev.as_view(), name = "add_dev")
 ]
