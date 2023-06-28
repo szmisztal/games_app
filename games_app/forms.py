7dd2ba9b6e3d8bfcf804f554bd5ce0d3e729cbe2
szmisztal <sz.misztal@gmail.com>
2023-06-28 14:05:12 +0200

    change form name fo developer

--- games_app/forms.py
+++ games_app/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 from .models import Game, DevelopmentStudio
 
-class NewGameForm(forms.ModelForm):
+class GameForm(forms.ModelForm):
     class Meta:
         model = Game
         fields = ['title', 'genre', 'short_description', 'premiere', 'developer', 'completed']
 
-class NewDevForm(forms.ModelForm):
+class DevForm(forms.ModelForm):
     class Meta:
         model = DevelopmentStudio
         fields = ['name']
