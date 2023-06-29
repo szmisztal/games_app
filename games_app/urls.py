6d0775859699369145cf7ed09034e7a169325347
szmisztal <sz.misztal@gmail.com>
2023-06-29 10:39:24 +0200

    add urls for delete game/dev

--- games_app/urls.py
+++ games_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomepageView, GamesListView, DevStudioListView, SingleGameView, SingleDevView, AddNewGame, AddNewDev, EditGame, EditDev
+from .views import HomepageView, GamesListView, DevStudioListView, SingleGameView, SingleDevView, AddNewGame, AddNewDev, EditGame, EditDev, DeleteGame, DeleteDev
 
 urlpatterns = [
     path("", HomepageView.as_view(), name = "homepage"),
@@ -10,5 +10,7 @@ urlpatterns = [
     path("add_game/", AddNewGame.as_view(), name = "add_game"),
     path("add_dev/", AddNewDev.as_view(), name = "add_dev"),
     path("edit_game/<int:pk>/", EditGame.as_view(), name = "edit_game"),
-    path("edit_dev/<int:pk>/", EditDev.as_view(), name = "edit_dev")
+    path("edit_dev/<int:pk>/", EditDev.as_view(), name = "edit_dev"),
+    path("delete_game/<int:pk>/", DeleteGame.as_view(), name = "delete_game"),
+    path("delete_dev/<int:pk>/", DeleteDev.as_view(), name = "delete_dev")
 ]
